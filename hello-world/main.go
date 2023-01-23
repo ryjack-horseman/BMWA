7ab973073261004b231ce552c114298247cf8e45
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-01-23 14:57:15 -0500

    Add Home, About and Divide
    Added Home About and Divide handlers and functions along with error response

--- hello-world/main.go
+++ hello-world/main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "hello world")
-		fmt.Printf("Number of Bytes written: %d\n", n)
-		if err != nil {
-			fmt.Println(err)
-		}
-	})
+const portNumber = ":8080"
 
-	http.ListenAndServe(":8080", nil)
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2 + 2 is %d", sum))
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot Divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0.0 {
+		return 0, errors.New("cannot divide by zero")
+	} else {
+		return x / y, nil
+	}
+}
+
+// addValues adds two integers and returns the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
+	fmt.Printf("Starting application on port %s", portNumber)
+	http.ListenAndServe(portNumber, nil)
 }

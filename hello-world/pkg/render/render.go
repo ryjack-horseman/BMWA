03047c71a734b91f50eeaefb1f4db1b9fc01048e
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-01-25 12:58:06 -0500

    Refactor templates
    Refactored common html into base.layout.html and updated render.go to use the boilplate

--- hello-world/pkg/render/render.go
+++ hello-world/pkg/render/render.go
@@ -6,9 +6,9 @@ import (
 	"net/http"
 )
 
-//renders templates using html/template
+// renders templates using html/template
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)

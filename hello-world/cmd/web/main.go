78c0cf86e928d3556c085b09cdff3584c55fc36e
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-01-27 16:01:51 -0500

    Added Basic cache and bug fix
    Added basic cache to limit .html reads from disk. Fixed bug where each interaction caused two log lines to be printed. The second log line comes from browser favicon request. Added handler that does nothing to prevent the catch-all from being called.

--- hello-world/cmd/web/main.go
+++ hello-world/cmd/web/main.go
@@ -12,6 +12,12 @@ const portNumber = ":8080"
 func main() {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
+
+	//added to ignore browser favicon request that results in an extra cache hit
+	http.HandleFunc("/favicon.ico", doNothing)
+
 	fmt.Printf("Starting application on port %s\n", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
 }
+
+func doNothing(w http.ResponseWriter, r *http.Request) {}
--- hello-world/pkg/render/render.go
+++ hello-world/pkg/render/render.go
@@ -3,15 +3,57 @@ package render
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 // renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error parsing template:", err)
 		return
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	//check to see if we already have the template in our cache
+	_, inMap := tc[t]
+
+	if !inMap {
+		log.Println("Creating template and adding to cache")
+		err = createTemplateCache(t)
+		if err != nil {
+			log.Println(err)
+		}
+	} else {
+		//we ahve the template in cache
+		log.Println("using cached template")
+	}
+
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.html",
+	}
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+	tc[t] = tmpl
+
+	return nil
 }

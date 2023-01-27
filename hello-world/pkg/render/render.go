06b24f8e7505b15cdfe4dfb49bf955b83e7ad572
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-01-27 18:03:03 -0500

    Refactor Cache
    Updated cache to remove hardcoded file paths, currently cache is removed completely as the createTemplateCache method is called each request

--- hello-world/pkg/render/render.go
+++ hello-world/pkg/render/render.go
@@ -1,59 +1,110 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 // renders templates using html/template
-func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.html")
-	err := parsedTemplate.Execute(w, nil)
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+
+	tc, err := createTemplateCache()
 	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
-}
-
-var tc = make(map[string]*template.Template)
-
-func RenderTemplate(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
-
-	//check to see if we already have the template in our cache
-	_, inMap := tc[t]
-
-	if !inMap {
-		log.Println("Creating template and adding to cache")
-		err = createTemplateCache(t)
-		if err != nil {
-			log.Println(err)
-		}
-	} else {
-		//we ahve the template in cache
-		log.Println("using cached template")
+		log.Fatal(err)
 	}
 
-	tmpl = tc[t]
-	err = tmpl.Execute(w, nil)
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
+
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = buf.WriteTo(w)
 	if err != nil {
 		log.Println(err)
 	}
 }
 
-func createTemplateCache(t string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s", t),
-		"./templates/base.layout.html",
-	}
-	tmpl, err := template.ParseFiles(templates...)
-	if err != nil {
-		return err
-	}
-	tc[t] = tmpl
+func createTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
 
-	return nil
+	//get all of the files named *.page.html from ./templates
+	pages, err := filepath.Glob("./templates/*.page.html")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.html")
+		if err != nil {
+			return myCache, nil
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.html")
+			if err != nil {
+				return myCache, nil
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
+
+// Naive caching mechanism, you must add additional templates to createTemplateCache as they are needed
+//var tc = make(map[string]*template.Template)
+
+// func RenderTemplate(w http.ResponseWriter, t string) {
+// 	var tmpl *template.Template
+// 	var err error
+
+// 	//check to see if we already have the template in our cache
+// 	_, inMap := tc[t]
+
+// 	if !inMap {
+// 		log.Println("Creating template and adding to cache")
+// 		err = createTemplateCache(t)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 	} else {
+// 		//we ahve the template in cache
+// 		log.Println("using cached template")
+// 	}
+
+// 	tmpl = tc[t]
+// 	err = tmpl.Execute(w, nil)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// }
+
+// func createTemplateCache(t string) error {
+// 	templates := []string{
+// 		fmt.Sprintf("./templates/%s", t),
+// 		"./templates/base.layout.html",
+// 	}
+// 	tmpl, err := template.ParseFiles(templates...)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	tc[t] = tmpl
+
+// 	return nil
+// }

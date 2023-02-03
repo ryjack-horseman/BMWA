1c5284a1f0a8c9af898926ca4a780e01a1902f3b
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-02-03 15:53:39 -0500

    Add Recover Middleware
    Helps for logging Panic

--- hello-world/cmd/web/routes.go
+++ hello-world/cmd/web/routes.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/ryjack-horseman/BMWA/hello-world/hello-world/pkg/config"
 	"github.com/ryjack-horseman/BMWA/hello-world/hello-world/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)

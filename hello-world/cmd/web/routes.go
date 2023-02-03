a61408ee8b93f50ca9d0ce3024f104bde50eb0c7
ryjack-horseman <44657668+ryjack-horseman@users.noreply.github.com>
2023-02-03 14:17:51 -0500

    Swap Router packages
    Use Chi instead of PAT for routing, want access to chi's middleware

--- hello-world/cmd/web/routes.go
+++ hello-world/cmd/web/routes.go
@@ -3,17 +3,16 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 	"github.com/ryjack-horseman/BMWA/hello-world/hello-world/pkg/config"
 	"github.com/ryjack-horseman/BMWA/hello-world/hello-world/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	mux.Get("/favicon", http.HandlerFunc(doNothing))
-
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/favicon", doNothing)
 	return mux
 }
